4631d46fd00bda70ec65e96e2d9192f4015413e5
Abu Salah Musha Lemon <lemonahmed512@gmail.com>
2024-09-06 12:24:32 +0600

    DB

--- stockGenie_final_exit/db/stockgenie.sql
+++ stockGenie_final_exit/db/stockgenie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2024 at 10:17 AM
+-- Generation Time: Sep 06, 2024 at 08:23 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `attendances` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `user_id` int(11) NOT NULL,
-    `att_time` time DEFAULT NULL,
+  `att_time` varchar(255) NOT NULL,
   `att_date` varchar(255) NOT NULL,
   `att_year` varchar(255) NOT NULL,
   `attendance` varchar(255) NOT NULL,
@@ -85,7 +85,6 @@ CREATE TABLE `category` (
 CREATE TABLE `customers` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -200,18 +199,18 @@ CREATE TABLE `migrations` (
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (8, '2024_06_10_101938_create_categories_table', 2),
-(73, '0001_01_01_000000_create_users_table', 3),
-(74, '0001_01_01_000001_create_cache_table', 3),
-(75, '0001_01_01_000002_create_jobs_table', 3),
-(76, '2024_06_10_091426_create_employees_table', 3),
-(77, '2024_06_10_101330_create_products_table', 3),
-(78, '2024_06_10_101526_create_customers_table', 3),
-(79, '2024_06_10_101736_create_suppliers_table', 3),
-(80, '2024_06_10_102130_create_orders_table', 3),
-(81, '2024_06_10_102250_create_order_details_table', 3),
-(82, '2024_06_10_102422_create_expenses_table', 3),
-(83, '2024_06_10_102600_create_attendances_table', 3),
-(84, '2024_06_10_180915_create_category_table', 3);
+(155, '2024_06_10_101526_create_customers_table', 4),
+(167, '0001_01_01_000000_create_users_table', 5),
+(168, '0001_01_01_000001_create_cache_table', 5),
+(169, '0001_01_01_000002_create_jobs_table', 5),
+(170, '2024_06_10_091426_create_employees_table', 5),
+(171, '2024_06_10_101330_create_products_table', 5),
+(172, '2024_06_10_101736_create_suppliers_table', 5),
+(173, '2024_06_10_102130_create_orders_table', 5),
+(174, '2024_06_10_102250_create_order_details_table', 5),
+(175, '2024_06_10_102422_create_expenses_table', 5),
+(176, '2024_06_10_102600_create_attendances_table', 5),
+(177, '2024_06_10_180915_create_category_table', 5);
 
 -- --------------------------------------------------------
 
@@ -311,7 +310,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('k5W0vfnJiIcu6WFDj83UxpPYP7bNwuUwRrZQ58nt', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoicUVDY1Jha1BkSDZ1RXA2aVRvbVRIYzhYaUh3YVhGOHBhMkJVRERDYiI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czozMjoiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2NhdGVnb3JpZXMiO31zOjk6Il9wcmV2aW91cyI7YToxOntzOjM6InVybCI7czozMToiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2Rhc2hib2FyZCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjE7fQ==', 1718093826);
+('BiGTDponeMwzomy4B8kmfrJhxlZ7zslYsRhPHdrK', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoicnlhZW45S3l2TzVRU3dqMEVlT1Z6OWJpUnpUWGFmNzI0bUZ1SXhWeSI7czozOiJ1cmwiO2E6MTp7czo4OiJpbnRlbmRlZCI7czozMzoiaHR0cDovLzEyNy4wLjAuMTo4MDAwL2FsbC1wcm9kdWN0Ijt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO30=', 1725603767);
 
 -- --------------------------------------------------------
 
@@ -352,7 +351,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'ASMLemon', 'lemonahmed065@gmail.com', NULL, '$2y$12$Y34AXRIVrs4wEnk2l/FFnutc/MQxHmZXuXWUEfkKyzs0RRl6Hmq5G', NULL, '2024-06-11 08:17:06', '2024-06-11 08:17:06');
+(1, 'Musha', 'abusalahmusha512@gmail.com', NULL, '$2y$12$2k2RUbEgUbP853AZ324IF.JgqTI.v9JME/Q8GvgTvY0x8IMwwY6vG', NULL, '2024-09-06 00:22:47', '2024-09-06 00:22:47');
 
 --
 -- Indexes for dumped tables
@@ -491,7 +490,7 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `employees`
@@ -521,7 +520,7 @@ ALTER TABLE `jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=178;
 
 --
 -- AUTO_INCREMENT for table `orders`
